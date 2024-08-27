3661a1aa87f62b9255b2ac1c881b5adbe8b8282f
alecknipe <alecknipe@gmail.com>
2024-08-26 22:29:19 -0400

    Added bootstrap css and js

---
+++ script.js
@@ -0,0 +1,8 @@
+let choiceOne = prompt("Would you like to sign in? (Y for Yes/N for No)");
+
+if(choiceOne === "Y" || choiceOne === "y") {
+   
+} else {
+    prompt("Please close the website if you do not want to sign in.");
+}
+   
